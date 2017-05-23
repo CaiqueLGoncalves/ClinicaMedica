aedbafd04031d528d60f6cd58327e0389d8b76d2
Murilo <murilow@gmail.com>
2017-05-23 16:18:44 -0300

    Ajustei o nome do Formulário na classe Program

--- ClinicaMedica/Program.cs
+++ ClinicaMedica/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClinicaMedica.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace ClinicaMedica
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmIndex());
         }
     }
 }
